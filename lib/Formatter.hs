4595f425e085ca010d91edb205eb6cfe2b7b5595
Hans Hoffman <github.backrest158@passmail.net>
2024-08-01 21:26:40 +1200

    initial formatter

--- lib/Formatter.hs
+++ lib/Formatter.hs
@@ -1,6 +1,20 @@
 module Formatter (format) where
 
 import Data.ByteString qualified as B
+import Data.ByteString.Char8 qualified as BC
+import Parser (JsonValue (..))
 
-format :: B.ByteString -> B.ByteString
-format = undefined
+format :: JsonValue -> B.ByteString
+format (JBool True) = "true"
+format (JBool False) = "false"
+format JNull = "null"
+format (JString bs) = bs
+format (JNumber num) = BC.pack $ show num
+format (JArray arr) =
+  "[ "
+    <> (B.intercalate ", " $ map format arr)
+    <> " ]"
+format (JObject pairs) =
+  "{\n"
+    <> foldl (\acc pair -> acc <> fst pair <> ":" <> format (snd pair)) "" pairs
+    <> "\n}"
