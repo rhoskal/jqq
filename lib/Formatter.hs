2b0eb49aabda35eddfe3f4c02200969d6ed80585
Hans Hoffman <github.backrest158@passmail.net>
2024-08-04 13:53:35 +1200

    add key/pair formatting

--- lib/Formatter.hs
+++ lib/Formatter.hs
@@ -5,17 +5,22 @@ import Data.ByteString.Char8 qualified as BC
 import Parser (JNum (..), JsonValue (..))
 
 format :: JsonValue -> B.ByteString
+format JNull = "null"
 format (JBool True) = "true"
 format (JBool False) = "false"
-format JNull = "null"
 format (JString str) = str
 format (JNumber (JInt num)) = BC.pack $ show num
 format (JNumber (JFloat num)) = BC.pack $ show num
 format (JArray arr) =
-  "[ "
-    <> B.intercalate ", " (map format arr)
-    <> " ]"
-format (JObject pairs) =
-  "{\n"
-    <> foldl (\acc pair -> acc <> fst pair <> ":" <> format (snd pair)) "" pairs
-    <> "\n}"
+  let values :: [JsonValue] -> B.ByteString
+      values [] = ""
+      values vs = B.intercalate ", " (map format vs)
+   in "[" <> values arr <> "]"
+format (JObject obj) =
+  let pairs :: [(B.ByteString, JsonValue)] -> B.ByteString
+      pairs [] = ""
+      pairs ps = B.intercalate ", " (map formatPair ps)
+
+      formatPair :: (B.ByteString, JsonValue) -> B.ByteString
+      formatPair (k, v) = BC.pack (show k) <> ": " <> format v
+   in "{" <> pairs obj <> "}"
